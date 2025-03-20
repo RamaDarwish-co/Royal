bdf75d1ef9df568b7aae451c536379a4b24c7f1b
RamaDarwish-co <ramihbib630@gmail.com>
2025-03-20 07:13:34 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <link rel="stylesheet" href="includes/head.php">
+    <?php include_once "includes/head.php"; ?> 
+   <title>Home page</title>
+   <link rel="stylesheet" href="styles/index.css">
+</head>
+
+<body>
+   <link rel="stylesheet" href="includes/header.php">
+   <?php include_once "includes/header.php"; ?> 
+
+
+   <div class="container flex-cen home">
+    
+      <h1>Royal is a distinctive destination to meet real estate needs <span id="span">home..</span> </h1>
+     
+   </div>
+   <!--  -->
+
+   <div class="skills flex-evn p25 g8">
+      <div class="card grid p20 g8">
+         <i class="fa-solid fa-home"></i>
+         <h2>Residential</h2>
+         <p>Our projects include a variety of residential properties located in the best areas in the UAE.</p>
+         <a href="residential.php" class="btn">List of our residential properties.<i class="fa-solid fa-share"></i></a>
+      </div>
+
+
+      <!--  -->
+      <div class="card grid p20 g8">
+         <i class="fa-solid fa-building"></i>
+
+         <h2>Commercial</h2>
+         <p>Our commercial projects in the UAE for sale or rent are offered in various sectors.</p>
+         <a href="commercial.php" class="btn">List of our commercial properties.<i class="fa-solid fa-share"></i></a>
+      </div>
+
+
+
+      <div class="card grid p30 g8">
+         <i class="fa-solid fa-sign-language"></i>
+
+         <h2>Our services</h2>
+         <p>We provide the best and latest real estate solutions and services that exceed your expectations</p>
+         <a href="service" class="btn">We provide our services<i class="fa-solid fa-share"></i></a>
+      </div>
+
+
+
+      <div class="card grid p35 g8">
+         <i class="fa-solid fa-address-book"></i>
+
+         <h2>Who are we</h2>
+         <p>Royal is a major award-winning real estate services company.</p>
+         <a href="around" class="btn">Get to know our company<i class="fa-solid fa-share"></i></a>
+      </div>
+      <!--  -->
+      <div class=" flex-evn p20 g25">
+         <div class="text">
+            <h1>With Royal :</h1>
+            <p>Our team of seasoned experts in the real estate sector
+               strives to provide a wide range of real estate solutions,
+               including sales, leasing, construction consultancy and others
+               with the aim of finding the property that suits your business.</p>
+         </div>
+         <div class="img">
+            <img src="images/deal.gif" alt="gif deal">
+         </div>
+      </div>
+      <!--  -->
+      <div class="info">
+         <h3>
+            <p class="terms">
+
+
+               </a>
+         </h3>
+         </p>
+
+      </div>
+
+      <div class="browsing flex-cen p25">
+         
+         <img src="images/63.jpg" alt="63">
+         <img src="images/12.jpg" alt="12">
+         <img src="images/91.jpg" alt="91">
+         <img src="images/61.jpg" alt="71">
+         <img src="images/77.jpg" alt="77">
+         <img src="images/25.jpg" alt="25">
+
+      </div>
+
+      <!--  -->
+
+      <div class="flex-evn p25 g25">
+
+         <div class="list">
+            <h2>Advantages of working with us :</h2>
+
+            <ul>
+
+               <li> <i class="fa-solid fa-check fa-shake"></i>Our team has extensive local and international experience.</li>
+               <li> <i class="fa-solid fa-check fa-shake"></i>We collaborate closely with reputable lenders and key stakeholders in the real estate sector in the region.</li>
+               <li> <i class="fa-solid fa-check fa-shake"></i>We are committed to providing the best services and achieving your goals professionally and effectively.</li>
+               <li> <i class="fa-solid fa-check fa-shake"></i>Our team, which consists of experienced experts in the realestate sector <br>,seeks to provide a wide rangr of real estate solutions</li>
+
+
+            </ul>
+         </div>
+
+      </div>
+      <div class="rotation flex-cen">
+         <img src="images/insurance.gif" alt="insurance.gif">
+         <img src="images/lease.gif" alt="lease.gif">
+         <img src="images/price.gif" alt="price.gif">
+         <img src="images/value.gif" alt="value.gif">
+      </div>
+   </div>
+
+<link rel="stylesheet" href="includes/footer.php">
+   <?php include_once "includes/footer.php"; ?> 
+</body>
+
+</html>
